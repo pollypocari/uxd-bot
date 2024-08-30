97e67f5f6126fb9d50bd9979320fcf825e0ca382
pollypocari <pollypolly.pan@gmail.com>
2024-08-30 19:30:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def handle_message(event):
     text = event.message.text
     if text.startswith(SOURCE_URL_PREFIX) and text.endswith(URL_SUFFIX):
         converted_url = convert_url(text)
-        response_message = RESPONSE_MESSAGE_TEMPLATE.format(url=converted_url).replace("\\n", "\n")
+        response_message = f"{RESPONSE_MESSAGE_TEMPLATE}\n-\n{converted_url}"
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=response_message)
